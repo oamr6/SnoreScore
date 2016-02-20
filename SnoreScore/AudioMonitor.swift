1c2e422304e61731c50b41b768a6e9cfcc60153a
Corey Matzat <corey@thematzats.com>
2016-02-20 06:37:45 -0600

    Improved Audio Monitor class

--- SnoreScore/AudioMonitor.swift
+++ SnoreScore/AudioMonitor.swift
@@ -13,7 +13,7 @@ import AVFoundation
 class AudioMonitor {
     
     var recorder: AVAudioRecorder!
-    var levelTimer = NSTimer()
+    var monitorTimer = NSTimer()
     var lowPassResults: Double = 0.0
     
     init() {
@@ -26,36 +26,44 @@ class AudioMonitor {
         
         // Setup the URL for the audio file
         let documents: AnyObject = NSSearchPathForDirectoriesInDomains( NSSearchPathDirectory.DocumentDirectory,  NSSearchPathDomainMask.UserDomainMask, true)[0]
-        let str =  documents.stringByAppendingPathComponent("recordTest.caf")
+        let str =  documents.stringByAppendingPathComponent("snoreRecording.m4a")
         let url = NSURL.fileURLWithPath(str as String)
         
         // make a dictionary to hold the recording settings so we can instantiate our AVAudioRecorder
-        let recordSettings: [String : AnyObject] = [AVFormatIDKey:Int(kAudioFormatAppleIMA4),
-            AVSampleRateKey:44100.0,
-            AVNumberOfChannelsKey:2,AVEncoderBitRateKey:12800,
-            AVLinearPCMBitDepthKey:16,
-            AVEncoderAudioQualityKey:AVAudioQuality.Max.rawValue]
+        let recordSettings: [String : AnyObject] = [AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+                                                    AVSampleRateKey: 12000.0,
+                                                    AVNumberOfChannelsKey: 1 as NSNumber,
+                                                    AVEncoderAudioQualityKey: AVAudioQuality.High.rawValue]
         
         // Instantiate an AVAudioRecorder
         do {
             try recorder = AVAudioRecorder(URL: url, settings: recordSettings)
-        } catch {
-        }
+        } catch { }
+        
         recorder.prepareToRecord()
         recorder.meteringEnabled = true
         
-        // Start recording
-        recorder.record()
-        
         // Instantiate a timer to be called with whatever frequency we want to grab metering values
-        self.levelTimer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("levelTimerCallback"), userInfo: nil, repeats: true)
+        monitorTimer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("monitorTimerCallback"), userInfo: nil, repeats: true)
         
     }
     
-    //This selector/function is called every time our timer (levelTime) fires
-    func levelTimerCallback() {
+    //This selector/function is called every time our timer (monitorTimer) fires
+    func monitorTimerCallback() {
         recorder.updateMeters()
         print(recorder.averagePowerForChannel(0))
+    }
+    
+    func stopMonitor() {
+        if (recorder.recording) {
+            recorder.stop();
+        }
+    }
+    
+    func startMonitor() {
+        if (!recorder.recording) {
+            recorder.record()
+        }
     }
     
     
