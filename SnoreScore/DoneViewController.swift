c3705afb58da4653ab152266df28c1255fb11b5b
MU IT Program <muitprogram@wirelessprvnat-172-17-66-32.near.illinois.edu>
2016-02-20 11:34:12 -0600

    Done View Controller

---
+++ SnoreScore/DoneViewController.swift
@@ -0,0 +1,41 @@
+//
+//  DoneViewController.swift
+//  SnoreScore
+//
+//  Created by MU IT Program on 2/20/16.
+//  Copyright © 2016 Corey Matzat. All rights reserved.
+//
+
+import UIKit
+
+class DoneViewController: UIViewController {
+
+    @IBOutlet weak var snoreCountLabel: UILabel!
+    var snoreCount: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        snoreCountLabel.text = snoreCount
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func goBackToStart(sender: AnyObject) {
+        navigationController?.popToRootViewControllerAnimated(true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
