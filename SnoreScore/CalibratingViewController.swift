98186d2dbb51c29b18fb4882cb874c9016a46a73
Corey Matzat <corey@thematzats.com>
2016-02-20 06:56:34 -0600

    Added an AudioMonitor to Calibrating View

--- SnoreScore/CalibratingViewController.swift
+++ SnoreScore/CalibratingViewController.swift
@@ -9,9 +9,18 @@
 import UIKit
 
 class CalibratingViewController: UIViewController {
+    
+    var monitor: AudioMonitor
+    
+    required init?(coder aDecoder: NSCoder) {
+        monitor = AudioMonitor()
+        super.init(coder: aDecoder)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        monitor.startMonitor()
 
         // Do any additional setup after loading the view.
     }
@@ -23,14 +32,15 @@ class CalibratingViewController: UIViewController {
     
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        //monitor.stopMonitor()
     }
-    */
+    
 
 }
