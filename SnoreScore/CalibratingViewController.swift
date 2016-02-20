b7f97d8c127171f624ed926cc51f17aa9fd5f8c4
MU IT Program <muitprogram@wirelessprvnat-172-17-66-32.near.illinois.edu>
2016-02-19 23:17:31 -0600

    Set up 2 Basic view Controllers, The start Controller and The calibrating Controller

---
+++ SnoreScore/CalibratingViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CalibratingViewController.swift
+//  SnoreScore
+//
+//  Created by MU IT Program on 2/19/16.
+//  Copyright © 2016 Corey Matzat. All rights reserved.
+//
+
+import UIKit
+
+class CalibratingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SnoreScore/StartScreenViewController.swift
@@ -0,0 +1,38 @@
+//
+//  StartScreenViewController.swift
+//  SnoreScore
+//
+//  Created by MU IT Program on 2/19/16.
+//  Copyright © 2016 Corey Matzat. All rights reserved.
+//
+
+import UIKit
+
+class StartScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
