8c4bd0e6a8b69ff46c834026c23fa356d8d7e496
MU IT Program <muitprogram@wirelessprvnat-172-17-111-160.near.illinois.edu>
2016-02-20 13:05:06 -0600

    still working on haptic

--- SnoreScore WatchKit Extension/InterfaceController.swift
+++ SnoreScore WatchKit Extension/InterfaceController.swift
@@ -8,9 +8,9 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
 
-
-class InterfaceController: WKInterfaceController {
+class InterfaceController: WKInterfaceController, WCSessionDelegate  {
 
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
@@ -21,11 +21,34 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        print("LOLOLOL")
+        if WCSession.isSupported() {
+            let session = WCSession.defaultSession()
+            session.delegate = self
+            session.activateSession()
+        }
     }
 
+    @IBAction func LastHapticFeedback() {
+        var timer = NSTimer()
+        timer = NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: "HapticFeedback", userInfo: nil, repeats: true)
+        timer.fire()
+        
+        WKInterfaceDevice.currentDevice().playHaptic(.Start)
+    }
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+
+   
+
+    
+    func session(session: WCSession, didReceiveMessage message: [String : AnyObject]) {
+        print("lollololololololo")
+        if ((message.keys.indexOf("vibrate")) != nil) {
+            self.LastHapticFeedback()
+        }
     }
 
 }
--- SnoreScore/SnoreRecorderViewController.swift
+++ SnoreScore/SnoreRecorderViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import WatchConnectivity
 
-class SnoreRecorderViewController: UIViewController {
+class SnoreRecorderViewController: UIViewController, WCSessionDelegate {
 
+    var session: WCSession?
     @IBOutlet weak var state: UILabel!
     @IBOutlet weak var buttonState: UIButton!
+    
     
     
     var count = 0
@@ -19,6 +22,16 @@ class SnoreRecorderViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //print(NSUserDefaults.standardUserDefaults().integerForKey("numberTimes"))
+        // Do any additional setup after loading the view, typically from a nib.
+        if WCSession.isSupported() {
+            session = WCSession.defaultSession()
+            guard let session = session else {
+                return
+            }
+            session.delegate = self
+            session.activateSession()
+        }
         buttonState.enabled = true
         buttonState.setTitle("Start Recording", forState: .Normal)
         buttonState.titleLabel?.text = "Start Recording"
@@ -26,6 +39,17 @@ class SnoreRecorderViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+  
+    @IBAction func Vibrate(sender: AnyObject) {
+        session!.sendMessage(["vibrate": true], replyHandler: { (reply) -> Void in
+            // do something
+            }) { (error) -> Void in
+                //
+                print("SOMETHING HAPPENEF \(error)")
+        }
+
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
